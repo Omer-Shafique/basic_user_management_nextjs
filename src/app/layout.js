f02916265c6c87823642f8cfe29f2f2a81f27459
Your Name <you@example.com>
2024-12-02 11:02:54 +0500

    Initial commit from Create Next App

---
+++ src/app/layout.js
@@ -0,0 +1,11 @@
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <h1>this is root layout</h1>
+        {children}
+      </body>
+    </html>
+  );
+}
